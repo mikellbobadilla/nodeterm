a114e36e432426d8bb16aa62c97632af37bf36b6
Sting <bobadilla413@gmail.com>
2022-05-16 23:46:44 -0300

    add commander module

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+const fs = require('fs')
+const cli = require('commander')
 const data = require('./data/setting.json')
 
-console.log(data.theme = 'light')
+const sizes = data.profiles.list.map(item => item.size)
+
+const path =
+  'C:\\Users\\Sting\\AppData\\Local\\Packages\\Microsoft.WindowsTerminal_8wekyb3d8bbwe\\LocalState'
+
+// fs.readdir(path, (err, files) => {
+//   if (err) {
+//     console.log(`Error: ${err}`)
+//   } else {
+//     console.log(files)
+//   }
+// })
+
+cli.description('Custimize your terminal')
+cli.name('nodeterm')
+cli.usage('<command>')
+cli.addHelpCommand(false)
+cli.helpOption(false)
+
+// Adding commands
+cli
+  .command('font')
+  .argument('[number]', 'The number of font size')
+  .description('Set font size')
+  .option('-s, --small', 'Set font size to small')
+  .action(fontOption)
+
+cli.parse(process.argv)
+
+function fontOption(number, options) {
+  
+  console.log(parseInt(number), options ,sizes)
+}
