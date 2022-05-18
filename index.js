b0a182507e10a4248d8d70b8b35978b195541812
Sting <bobadilla413@gmail.com>
2022-05-18 17:51:12 -0300

    new module added commander deleted

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
-const fs = require('fs')
-const cli = require('commander')
-const data = require('./data/setting.json')
+const parser = require('./controllers/commands.js')
 
-const sizes = data.profiles.list.map(item => item.size)
+function app() {
+  console.dir(parser)
+}
 
-const path =
-  'C:\\Users\\Sting\\AppData\\Local\\Packages\\Microsoft.WindowsTerminal_8wekyb3d8bbwe\\LocalState'
+app()
 
-// fs.readdir(path, (err, files) => {
-//   if (err) {
-//     console.log(`Error: ${err}`)
-//   } else {
-//     console.log(files)
-//   }
+// parser.add_argument('-v', '--version', { action: 'version', version })
+// parser.add_argument('-s', '--size', {
+//   help: 'set font size (default: 12)',
+//   type: 'int',
 // })
 
-cli.description('Custimize your terminal')
-cli.name('nodeterm')
-cli.usage('<command>')
-cli.addHelpCommand(false)
-cli.helpOption(false)
+// parser.add_argument('-b', {
+//   help: 'Add a second number',
+//   type: 'int',
+//   action: 'store',
+// })
 
-// Adding commands
-cli
-  .command('font')
-  .argument('[number]', 'The number of font size')
-  .description('Set font size')
-  .option('-s, --small', 'Set font size to small')
-  .action(fontOption)
+// // function num(n) {
+// //   return n
+// // }
 
-cli.parse(process.argv)
+// // process.stdin.write(parser.parse_args())
+// const arg = parser.parse_args()
 
-function fontOption(number, options) {
-  
-  console.log(parseInt(number), options ,sizes)
-}
+// function font({ s, size }) {
+//   return `font ${s || size || 12}`
+// }
+
+// // console.dir(arg.a + arg.b)
+// console.log(font(arg))
+
+// console.dir(h)
